4fe0ea105fb2a6042fbc3b7a6bfb5beb8d011e9d
jakub.masek <masek377@gmail.com>
2019-07-24 08:11:48 +0200

    implementace Tester1

--- src/main/java/cz/actis/tutorial/testers/Tester1.java
+++ src/main/java/cz/actis/tutorial/testers/Tester1.java
@@ -17,26 +17,34 @@ public class Tester1 {
      * Naimplementuj public metodu minus, ktera bude odecitat 2 cisla (a - b). Parametry budou int a int
      * Vracet se bude int
      */
-
+    public int minus(int a, int b){
+        return a - b;
+    }
     /**
      * Zadani
      * Naimplementuj public metodu times, ktera bude nasobit 2 cisla. Parametry budou int a int
      * Vracet se bude int
      */
-
+    public int times(int a, int b) {
+        return a * b;
+    }
 
     /**
      * Zadani
      * Naimplementuj public metodu divide, ktera bude delit 2 cisla. Parametry budou int a int
      * Vracet se bude double
      */
-
+    public double divide(int a, int b){
+        return (double) a / (double) b;
+    }
 
     /**
      * Zadani
      * Naimplementuj public metodu mod, ktera vrati zbytek po deleni. Parametry budou int a int
      * Vracet se bude int
      */
-
+    public int mod(int a, int b) {
+        return a % b;
+    }
 
 }
