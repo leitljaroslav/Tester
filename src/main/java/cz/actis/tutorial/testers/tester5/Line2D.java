61f2e41b0a1b33905e9b8d4d159b432c4bfe2c1c
jakub.masek <masek377@gmail.com>
2019-07-31 11:10:44 +0200

    implementace Tester4

--- src/main/java/cz/actis/tutorial/testers/tester5/Line2D.java
+++ src/main/java/cz/actis/tutorial/testers/tester5/Line2D.java
@@ -3,6 +3,8 @@ package cz.actis.tutorial.testers.tester5;
 import cz.actis.tutorial.testers.tester4.Point;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.text.DecimalFormat;
+
 public class Line2D {
     /**
      * Zadání: V balíku tester5 vytvoř třídu Line2D.
@@ -34,33 +36,39 @@ public class Line2D {
     public boolean isPerpendicular(Line2D line) {
         double x1 = line.b.getX() - line.a.getX();
         double y1 = line.b.getY() - line.a.getY();
-        double soucetVectoru1 = x1 * x1 + y1 * y1;
-        double velikostVectoru1 = Math.sqrt(soucetVectoru1);
+        double sumVector1 = x1 * x1 + y1 * y1;
+        double sizeVector1 = Math.sqrt(sumVector1);
         double x2 = b.getX() - a.getX();
         double y2 = b.getY() - a.getY();
-        double soucetVectoru2 = x2 * x2 + y2 * y2;
-        double velikostVectoru2 = Math.sqrt(soucetVectoru2);
-        double skalar = x1 * x2 + y1 * y2;
-        double pomer = skalar / (velikostVectoru1 * velikostVectoru2);
-        double cosUhlu = Math.cos(pomer);
-        return cosUhlu == 1;
+        double sumVector2 = x2 * x2 + y2 * y2;
+        double sizeVector2 = Math.sqrt(sumVector2);
+        double scalarProduct = x1 * x2 + y1 * y2;
+        double ratio = scalarProduct / (sizeVector1 * sizeVector2);
+        return ratio == 0;
 
     }
 
     public boolean isParallel(Line2D line) {
-        throw new NotImplementedException();
-
+        double u1 = line.b.getX() - line.a.getX();
+        double u2 = line.b.getY() - line.a.getY();
+        double v1 = b.getX() - a.getX();
+        double v2 = b.getY() - a.getY();
+        return (u1 / v1) == (u2 / v2);
     }
 
     public double getAngle(Line2D line) {
-        throw new NotImplementedException();
-    }
-
-    public Point getB() {
-        return b;
-    }
-
-    public void setB(Point b) {
-        this.b = b;
+        double x3 = line.b.getX() - line.a.getX();
+        double y3 = line.b.getY() - line.a.getY();
+        double sumVector3 = x3 * x3 + y3 * y3;
+        double sizeVector3 = Math.sqrt(sumVector3);
+        double x4 = b.getX() - a.getX();
+        double y4 = b.getY() - a.getY();
+        double sumVector4 = x4 * x4 + y4 * y4;
+        double sizeVector4 = Math.sqrt(sumVector4);
+        double scalarProduct1 = x3 * x4 + y3 * y4;
+        double cosAngle = scalarProduct1 / (sizeVector3 * sizeVector4);
+        double angle = Math.acos(cosAngle);
+        double result = Math.toDegrees(angle);
+        return ((int)(result * 1000)) /1000d;
     }
 }
